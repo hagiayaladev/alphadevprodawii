168cb910891038be14bbce53a6eebf5eade88924
Hagi Ayala <hriskart@gmail.com>
2024-06-27 07:50:56 -0500

    DAWII PROJECT

---
+++ src/main/java/com/alfadevpro/AlfadevproDawiiApplication.java
@@ -0,0 +1,13 @@
+package com.alfadevpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfadevproDawiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfadevproDawiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alfadevpro/AlfadevproDawiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfadevpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfadevproDawiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
